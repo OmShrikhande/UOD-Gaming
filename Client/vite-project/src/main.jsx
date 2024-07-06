d9789545ae24ebe611ff1627bea6e19912f586df
UDMOB <gawandeuday9@gmail.com>
2024-07-06 15:20:10 +0530

    body

---
+++ Client/vite-project/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App.jsx';
+import './index.css';
+
+// Use ReactDOM.createRoot to render the app
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
