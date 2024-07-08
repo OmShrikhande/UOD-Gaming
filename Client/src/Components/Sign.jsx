ba432e94f7f4e0a7f9a677e052388e1620a1c80c
Dev :) <devashishpillay777@gmail.com>
2024-07-08 11:50:41 +0530

    Update Sign.jsx

--- Client/src/Components/Sign.jsx
+++ Client/src/Components/Sign.jsx
@@ -1,57 +1,64 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "../Css/Sign.css"; 
 import Foote from './Foote';
+import axios from 'axios';
 
-const Sign = ({ onSign }) => {
+const Sign = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSign = (e) => {
+  const handleSign = async (e) => {
     e.preventDefault();
-    
+
     const newUser = {
       username,
       email,
       password
     };
-    onSign(newUser); 
-    alert('Signed up successfully!');
-    setUsername('');
-    setEmail('');
-    setPassword('');
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/v1/user/signup', newUser);
+      alert('Signed up successfully!');
+      setUsername('');
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error('There was an error signing up!', error);
+      alert('Sign up failed. Please try again.');
+    }
   };
 
   return (
     <div>
-    <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSign}>
-        <h2>Sign </h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Sign </button>
-      </form>
-    </div>
-    <Foote/>
+      <div className="signup-container">
+        <form className="signup-form" onSubmit={handleSign}>
+          <h2>Sign</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Sign</button>
+        </form>
+      </div>
+      <Foote />
     </div>
   );
 };
