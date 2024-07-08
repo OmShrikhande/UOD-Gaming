e6079808e3252b2743ef5d2236a5ebc4e46802cb
Dev :) <devashishpillay777@gmail.com>
2024-07-08 12:25:47 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRouter from "./Routes/auth.route.js"; 
+import gameRouter from "./Routes/game.route.js"; 
+
 dotenv.config({
     path: "./.env",
 });
@@ -10,20 +13,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.set('trust proxy', true); 
-
-import userRouter from "./Routes/auth.route.js";
-import gameRouter from "./Routes/game.route.js"; 
+app.set('trust proxy', true);
 
 app.use("/api/v1/user", userRouter);
-app.use("/api/v2/game", gameRouter); 
+app.use("/api/v2/game", gameRouter);
 
 const port = process.env.PORT;
 
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => app.listen(port))
-    .then(() =>
-        console.log(`⚙️  Server is running and connected to db at port ${port} :)`)
-    )
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(port, () => console.log(`⚙️  Server is running and connected to db at port ${port} :)`)))
     .catch((err) => console.log(`${err} is error`));
